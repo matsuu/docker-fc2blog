183e5f4526080f80dc55afd151f03fea2f79da0a
MATSUU Takuto <matsuu@gmail.com>
2014-03-20 23:26:41 +0900

    Initial import.

--- README.md
+++ README.md
@@ -1,4 +1,41 @@
-docker-fc2blog
+ocker-fc2blog
 ==============
 
-Dockerfiles for fc2blog
+Dockerfile for fc2blog
+
+#### Requirements
+
+* Docker
+
+#### Installation (CentOS)
+
+    docker pull centos
+    git clone http://github.com/matsuu/docker-fc2blog
+    cd docker-fc2blog
+    cd centos
+    vi run.sh
+    sh run.sh
+
+#### Installation (Ubuntu)
+
+    docker pull ubuntu
+    git clone http://github.com/matsuu/docker-fc2blog
+    cd docker-fc2blog
+    cd ubuntu
+    vi run.sh
+    sh run.sh
+
+#### See also
+
+* [fc2blog/blog](https://github.com/fc2blog/blog)
+* [Docker](https://www.docker.io/)
+
+#### TODO
+
+* Unlink /admin/install.php after installation
+* Support other distributions
+* Support log handling
+* Execute mysql_secure_install
+* Support MySQL Master-Slave
+* Integrate with etcd+fleet for CoreOS
+* Support Nginx
---
+++ ubuntu/run.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+DOMAIN="${DOMAIN:-localhost}"
+DB_USER="${DB_USER:-fc2blog}"
+DB_PASSWORD="${DB_PASSWORD:-fc2blog}"
+DB_DATABASE="${DB_DATABASE:-fc2blog}"
+PASSWORD_SALT="${PASSWORD_SALT:-fc2blog}"
+HTTP_PORT="80"
+
+DB_NAME="mysql"
+WEB_NAME="apache"
+WEB_INTERNAL_PORT="80"
+
+CWD=`dirname $0`
+DATE=`date +%Y%m%d%H%M%S`
+DB_TAG="fc2blog/${DB_NAME}:${DATE}"
+WEB_TAG="fc2blog/${WEB_NAME}:${DATE}"
+DB_INSTANCE_NAME="${DB_NAME}-${DATE}"
+WEB_INSTANCE_NAME="${WEB_NAME}-${DATE}"
+
+cat "${CWD}/${DB_NAME}/Dockerfile" | sed \
+  -e "s/@DB_USER@/$DB_USER/" \
+  -e "s/@DB_PASSWORD@/$DB_PASSWORD/" \
+  -e "s/@DB_DATABASE@/$DB_DATABASE/" \
+  | docker build --tag="${DB_TAG}" -
+cat "${CWD}/${WEB_NAME}/Dockerfile" | sed \
+  -e "s/@DOMAIN@/$DOMAIN/" \
+  -e "s/@DB_USER@/$DB_USER/" \
+  -e "s/@DB_PASSWORD@/$DB_PASSWORD/" \
+  -e "s/@DB_DATABASE@/$DB_DATABASE/" \
+  | docker build --tag="${WEB_TAG}" -
+docker run --name="${DB_INSTANCE_NAME}" --detach "${DB_TAG}"
+docker run --publish="${HTTP_PORT}:${WEB_INTERNAL_PORT}" --name="${WEB_INSTANCE_NAME}" --link="${DB_INSTANCE_NAME}:${DB_NAME}" --detach "${WEB_TAG}"
+
+HOST_PORT=`docker port "${WEB_INSTANCE_NAME}" "${WEB_INTERNAL_PORT}"`
+echo "http://${HOST_PORT}/admin/install.php"
